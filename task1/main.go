8e85b679285b1609e4b5013aa266752cbfd11fc8
Bogban893 <bogbanrvd@gmail.com>
2026-03-09 16:52:13 +0300

    fix error handling

--- task1/main.go
+++ task1/main.go
@@ -17,42 +17,48 @@ type RepoInfo struct {
 
 func request(owner string, repo string) (*RepoInfo, error) {
 	url := "https://api.github.com/repos/" + owner + "/" + repo
+
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return nil, fmt.Errorf("ошибка создания запроса: %w", err)
+		return nil, fmt.Errorf("failed to create request: %w", err)
 	}
+	req.Header.Set("User-Agent", "go-client")
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return nil, fmt.Errorf("ошибка создания запроса: %w", err)
+		return nil, fmt.Errorf("request error: %w", err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("неожиданный статус: %s", resp.Status)
+		switch resp.StatusCode {
+		case http.StatusNotFound:
+			return nil, fmt.Errorf("repository not found")
+		case http.StatusForbidden:
+			return nil, fmt.Errorf("access forbidden or rate limit exceeded")
+		default:
+			return nil, fmt.Errorf("unexpected http status: %s", resp.Status)
+		}
 	}
 
 	var res RepoInfo
 	if err := json.NewDecoder(resp.Body).Decode(&res); err != nil {
-		return nil, fmt.Errorf("неожиданный статус: %s", resp.Status)
+		return nil, fmt.Errorf("failed to decode json: %w", err)
 	}
 
 	return &res, nil
 }
 
-// Подумать над добавлением токена для запросов
 func main() {
 	if len(os.Args) != 3 {
-		fmt.Println("Некорректный ввод")
+		fmt.Println("Incorrect input")
 		return
 	}
 	res, err := request(os.Args[1], os.Args[2])
 	if err != nil {
-		fmt.Println(":((")
+		fmt.Println(err)
 		return
 	}
-	fmt.Printf("Репозиторий: %s\nОписание: %s\nЗвёзд: %d\nФорков: %d\nСоздан: %s\n",
+	fmt.Printf("Repository: %s\nDescription: %s\nStars: %d\nForks: %d\nCreated: %s\n",
 		res.Name, res.Description, res.Stars, res.Forks, res.Date)
-
-	fmt.Println(":)")
 }
