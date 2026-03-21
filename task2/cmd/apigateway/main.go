ef56e275c825f5a0d3647025098f94cc5844f97a
Kyriell <kirya.shmidt.0600@mail.ru>
2026-03-21 23:32:30 +0300

    feat:add comments for swagger in main and add endpoint for swagger in run_app

--- task2/cmd/apigateway/main.go
+++ task2/cmd/apigateway/main.go
@@ -2,6 +2,11 @@ package main
 
 import apigatewayapp "github.com/Chaice1/golang-course/task2/internal/apigateway/app"
 
+// @title           GitHub Collector API
+// @version         1.0
+// @description     API Gateway for GitHub Repository Information.
+// @host            localhost:8080
+// @BasePath        /
 func main() {
 	config := apigatewayapp.Config{
 		GRPCport: "collector:8082",
--- task2/internal/apigateway/app/run_app.go
+++ task2/internal/apigateway/app/run_app.go
@@ -3,11 +3,14 @@ package apigatewayapp
 import (
 	"log"
 
+	_ "github.com/Chaice1/golang-course/task2/docs"
 	collectorpb "github.com/Chaice1/golang-course/task2/gen"
 	apigatewayclient "github.com/Chaice1/golang-course/task2/internal/apigateway/adapters/client"
 	apigatewayhandler "github.com/Chaice1/golang-course/task2/internal/apigateway/adapters/handler"
 	apigatewayusecase "github.com/Chaice1/golang-course/task2/internal/apigateway/usecase"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
@@ -30,7 +33,8 @@ func RunApp(config Config) {
 
 	server := gin.Default()
 
-	server.GET("/get_repo_info", apigatewayHandler.GetRepoInfo)
+	server.GET("/get_repo_info/:owner/:repo", apigatewayHandler.GetRepoInfo)
+	server.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	if err := server.Run(config.HTTPport); err != nil {
 		log.Fatal(err)
 	}
